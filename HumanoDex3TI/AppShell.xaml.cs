0e88e09377afb76d5310a5e18f869aa141f8f2f6
Marcos Rocha <zooinm@hotmail.com>
2025-11-13 14:04:15 -0300

    Adicionar arquivos de projeto.

---
+++ HumanoDex3TI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HumanoDex3TI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HumanoDex3TI/Model/Humano.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanoDex3TI.Model
+{
+    public class Humano
+    {
+        private int id;
+        private string nome;
+        private decimal peso;
+        private decimal altura;
+        private string genero;
+        private string foto;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public decimal Peso { get => peso; set => peso = value; }
+        public decimal Altura { get => altura; set => altura = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Foto { get => foto; set => foto = value; }
+
+        public string salvaHumano(Humano humano)
+        {
+            return "Salvo com sucesso!";
+        }
+
+        public string deletaHumano(Humano humano)
+        {
+            return "deletado com sucesso!";
+        }
+        public string alteraHumano(Humano humano)
+        {
+            return "alterado com sucesso!";
+        }
+        public string buscaHumano(Humano humano)
+        {
+            return "encontrado com sucesso!";
+        }
+        public string buscaHumanoporId(Humano humano)
+        {
+            return "encontrado com sucesso!";
+        }
+    }
+
+   
+}
